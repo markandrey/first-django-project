4f5a7131ed9e7500b0b8b8953b37f75b0be296cc
progr1 <progr1@iMac-Irina.local>
2023-08-18 08:30:39 +0300

    lesson#4 Django

--- coolsite/patient/models.py
+++ coolsite/patient/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    surname = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    patronymic = models.CharField(max_length=100)
+    data_photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
